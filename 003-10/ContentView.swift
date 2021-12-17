ab9ada400dddeb2dd64c80def0b89a0411f4c102
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-17 21:16:20 +0200

    Initial commit

--- 003-10/ContentView.swift
+++ 003-10/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
+            }
+            .navigationBarTitle(Text("SwiftUI lesson 2"), displayMode: .inline)
+        }
     }
 }
 
